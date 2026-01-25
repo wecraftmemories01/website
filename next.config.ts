0d890a2958724cfadd55df31640fb9835d0af9aa
We Craft Memories <wecraftmemories01@gmail.com>
2026-01-26 00:53:14 +0530

    fix: prod api whitelisted to get images

--- next.config.ts
+++ next.config.ts
@@ -6,10 +6,18 @@ const nextConfig: NextConfig = {
     },
     images: {
         remotePatterns: [
+            // ✅ Local development
             {
                 protocol: "http",
                 hostname: "localhost",
                 port: "3000",
+                pathname: "/uploads/**",
+            },
+
+            // ✅ Production API
+            {
+                protocol: "https",
+                hostname: "api.wecraftmemories.com",
                 pathname: "/uploads/**",
             },
         ],
